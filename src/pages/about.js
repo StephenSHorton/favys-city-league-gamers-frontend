8029062b7fe52cca5989f403fdfeba232b4da6c8
MrHorton15 <4stephenhorton@gmail.com>
2019-11-05 10:54:01 -0700

    Fixed about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,12 +8,12 @@ const About = () => {
           <div>We are making a community of gamers!</div>
           <div className="content">
             <ul className="intro">We want to:</ul>
-            <li>-Create a community of city league gamers</li>
-            <li>-Create profiles to show off your skills </li>
-            <li>-Create teams and Clans for city league tournaments. </li>
-            <li>-Create tournaments for all city league gamers</li>
-            <li>-Publish updates for games</li>
-            <li>-Show games that are coming out soon.</li>
+            <li>Create a community of city league gamers</li>
+            <li>Create profiles to show off your skills </li>
+            <li>Create teams and Clans for city league tournaments. </li>
+            <li>Create tournaments for all city league gamers</li>
+            <li>Publish updates for games</li>
+            <li>Show games that are coming out soon.</li>
           </div>
         </div>
       </div>
