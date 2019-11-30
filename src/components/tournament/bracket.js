2c4aa381063f5ef733ebddb8129cef81fdb032c1
Stephen Horton <54323940+StephenSHorton@users.noreply.github.com>
2019-11-30 12:46:14 -0800

    Update bracket.js

--- src/components/tournament/bracket.js
+++ src/components/tournament/bracket.js
@@ -1,9 +1,38 @@
 import React from "react";
 
-const Bracket = () => {
+const Bracket = props => {
   return (
     <div className="bracket-container">
-      <h1>Content</h1>
+      <h1>{`${props.match}`}</h1>
+      <div className="content">
+        <div className="layer-1">
+          <div className="player-slot">
+            <div>Player 1</div>
+          </div>
+          <div className="player-slot">
+            <div>Player 2</div>
+          </div>
+          <div className="player-slot">
+            <div>Player 3</div>
+          </div>
+          <div className="player-slot">
+            <div>Player 4</div>
+          </div>
+        </div>
+        <div className="layer-2">
+          <div className="player-slot">
+            <div></div>
+          </div>
+          <div className="player-slot">
+            <div></div>
+          </div>
+        </div>
+        <div className="winner">
+          <div className="player-slot">
+            <div></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
